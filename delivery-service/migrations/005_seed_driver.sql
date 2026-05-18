7820dea4d7f54fa11c85cfa07f53ebd7bf7bd868
Nikita Vassilenko <vassilenko.n@kzns.kz>
2026-05-18 18:06:54 +0500

    fix(migrations): seed driver with is_available = true

--- delivery-service/migrations/005_seed_driver.sql
+++ delivery-service/migrations/005_seed_driver.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
-INSERT INTO drivers (id, user_id, name, email, phone) OVERRIDING SYSTEM VALUE VALUES
-  (1, 3, 'Test Driver', 'delivery@test.kz', '+77003334455')
+INSERT INTO drivers (id, user_id, name, email, phone, is_available) OVERRIDING SYSTEM VALUE VALUES
+  (1, 3, 'Test Driver', 'delivery@test.kz', '+77003334455', TRUE)
 ON CONFLICT DO NOTHING;
 
 SELECT setval(pg_get_serial_sequence('drivers', 'id'), GREATEST((SELECT MAX(id) FROM drivers), 1));
